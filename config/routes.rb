9480ba676865adf7183a224ada920a952d8262aa
Albert0ML17 <92704550+Albert0ML17@users.noreply.github.com>
2022-09-20 15:51:19 -0400

    arreglos

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :amigos
   #get 'home/index'
-  get 'home/quienes'
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
